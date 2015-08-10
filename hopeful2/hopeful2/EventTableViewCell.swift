d5115798c9e096a20504d111741003a3ed889590
tonymu <tony.mu101999@gmail.com>
2015-08-09 23:38:50 -0500

    flip isn't working

---
+++ hopeful2/hopeful2/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  hopeful2
+//
+//  Created by Mu Tong on 8/8/15.
+//  Copyright (c) 2015 Tony Mu. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+        
+    }
+
+}
--- hopeful2/hopeful2/EventTableViewController.swift
+++ hopeful2/hopeful2/EventTableViewController.swift
@@ -4,7 +4,10 @@ import UIKit
 class EventTableViewController: UITableViewController, UIScrollViewDelegate, UITableViewDelegate {
 
     @IBOutlet weak var pullDownLabel: UILabel!
-
+    
+    var eventPhotoView: UIImageView!
+    var eventDetailView: UIView!
+    
     var isAtTop = true
     
     override func viewDidLoad() {
@@ -38,11 +41,11 @@ class EventTableViewController: UITableViewController, UIScrollViewDelegate, UIT
     }
     
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 5
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 5
     }
 
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
@@ -51,30 +54,35 @@ class EventTableViewController: UITableViewController, UIScrollViewDelegate, UIT
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("EventCell", forIndexPath: indexPath) as! UITableViewCell
+        
+//        self.eventPhotoView = UIView(frame: cell.frame)
+//        cell.addSubview(eventPhotoView)
     
+        
+        
         if(indexPath.row <= 2) {
-            let backView = UIImageView(frame: cell.bounds)
-            let frontViewContainer = UIView(frame: backView.bounds)
+            let eventPhotoView = UIImageView(frame: cell.bounds)
+            let frontViewContainer = UIView(frame: eventPhotoView.bounds)
             let frontView = UIImageView(frame: frontViewContainer.bounds)
             
             frontViewContainer.frame.size = CGSize(width: 0, height: frontViewContainer.frame.size.height)
             
-            cell.addSubview(backView)
-            backView.addSubview(frontViewContainer)
+            cell.addSubview(eventPhotoView)
+            eventPhotoView.addSubview(frontViewContainer)
              frontViewContainer.addSubview(frontView)
             
             frontView.image = UIImage(named: "download.jpeg")
-            backView.image = UIImage(named: "download2.jpg")
+            eventPhotoView.image = UIImage(named: "download2.jpg")
             
             frontView.contentMode = UIViewContentMode.ScaleAspectFill
-            backView.contentMode = UIViewContentMode.ScaleAspectFill
+            eventPhotoView.contentMode = UIViewContentMode.ScaleAspectFill
             
             frontView.clipsToBounds = true
-            backView.clipsToBounds = true
+            eventPhotoView.clipsToBounds = true
             frontViewContainer.clipsToBounds = true
             
             UIView.animateWithDuration(6.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut | UIViewAnimationOptions.Autoreverse, animations: {
-                frontViewContainer.frame.size = CGSize(width: backView.frame.size.width, height: frontViewContainer.frame.size.height)
+                frontViewContainer.frame.size = CGSize(width: eventPhotoView.frame.size.width, height: frontViewContainer.frame.size.height)
                 }, completion: nil)
             
         } else {
@@ -86,6 +94,18 @@ class EventTableViewController: UITableViewController, UIScrollViewDelegate, UIT
         }
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        println("\(indexPath.row)")
+
+        if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+//            UIView.transitionFromView(eventPhotoView, toView: eventDetailView, duration: 1.0, options: UIViewAnimationOptions.TransitionFlipFromRight, completion: nil)
+//            UIView.transitionWithView(cell.contentView, duration: 1.0, options: UIViewAnimationOptions.TransitionFlipFromRight, animations: {
+//
+//                }, completion: nil)
+        }
     }
     
     func createEvent() {
